4df84de1e82e403c785b23f2a140ce3e199c95b8
Replit user <>
2022-11-06 12:08:07 +0000

    ignoreBuildErrors TS

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,7 +1,8 @@
 // @ts-ignore
+import { FC } from 'react';
 import Nav from '../components/Nav/Nav'
 
-const Layout = (props) => (
+const Layout:FC = (props) => (
   <>
   <Nav />
   <div className="container mx-auto mt-7 gap-7">
